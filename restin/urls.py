63daf1a5cff2f3ba2bfe797701fcc2e2f78a2bee
Andrés Camilo Mazo Hernandez <acmazoh@eafit.edu.co>
2026-03-18 20:33:50 -0500

    Revert "feat(auth): login and validation of user credentials"

--- restin/urls.py
+++ restin/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='venta_list', permanent=False)),
     path("admin/", admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('products/', include('products.urls')),
     path('ventas/', include('ventas.urls')),
 ]
